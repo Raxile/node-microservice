3aa83a3a89d43ecd7a3d099d9255da27ee941887
Piyush Saxena <piyushsaxena@Piyushs-MacBook-Air.local>
2025-05-22 10:39:35 +0530

    On branch feature/add-prettier-eslint-husky
     - add eslint prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,16 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 80,
+  endOfLine: 'auto',
+};
+
+export default config;
