914cd9c420651167a4de8cc5f65e191ca6110d6b
Piyush Saxena <piyushsaxena@Piyushs-MacBook-Air.local>
2025-05-25 02:59:12 +0530

    On branch feature/add-prisma  - add prisma and connect mongodb and postgress

--- services/auth-service/app.ts
+++ services/auth-service/app.ts
@@ -1,7 +1,11 @@
 import 'dotenv/config';
 import express, { Express } from 'express';
 import mainRoutes from './routes';
-import connectPostgresDB from './configs/postgres-db.config';
+import {
+  connectAllPrismaDB,
+  disconnectAllPrismaDB,
+} from './configs/prisma-db.config';
+// import connectPostgresDB from './configs/postgres-db.config';
 import connectMongoDB from './configs/mongo-db.config';
 
 const app: Express = express();
@@ -14,10 +18,17 @@ app.use('/', mainRoutes);
 (async () => {
   try {
     await connectMongoDB();
-    await connectPostgresDB();
+    // await connectPostgresDB();
+
+    await connectAllPrismaDB();
 
     app.listen(port, () => {
       console.log(`[server]: Server is running at http://localhost:${port}`);
+    });
+    process.on('SIGINT', async () => {
+      console.log('application shutdown');
+      await disconnectAllPrismaDB();
+      process.exit(0);
     });
   } catch (error) {
     console.log('❌ Some error occurred while connecting to DBs:', error);
